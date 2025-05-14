8bd6a8db2b471b310676fab61fb3dd2cb06772ca
sajeetharan <sajeetharan@outlook.com>
2025-05-14 15:30:47 +0530

    Update Game

---
+++ Assets/Scripts/Police.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Police : MonoBehaviour
+{
+    public float moveSpeed = 0.2f;
+    public float magnitude = 4.5f;
+    private Vector3 startLocation;
+
+    private void Start()
+    {
+        startLocation = transform.position;
+    }
+
+    private void Update()
+    {
+        var position = startLocation;
+        position.x += Mathf.Sin(Time.time * moveSpeed) * magnitude;
+        transform.position = position;
+    }
+}
